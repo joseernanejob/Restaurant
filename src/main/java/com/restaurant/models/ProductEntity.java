e972ed51ccfc8a0b1f6ff8e1d96f450a81ca2630
joseernanejob <jose.ernane.job@gmail.com>
2024-01-26 10:47:59 -0300

    feat: add Getters/Setters and constructors

--- src/main/java/com/restaurant/models/ProductEntity.java
+++ src/main/java/com/restaurant/models/ProductEntity.java
@@ -1,7 +1,15 @@
 package com.restaurant.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "products")
 public class ProductEntity {
