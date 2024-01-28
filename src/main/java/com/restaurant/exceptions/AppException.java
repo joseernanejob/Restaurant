02eac4120c73515db4bd7f3004fd78a56bb563c0
joseernanejob <jose.ernane.job@gmail.com>
2024-01-28 12:27:57 -0300

    feat: add AppException

---
+++ src/main/java/com/restaurant/exceptions/AppException.java
@@ -0,0 +1,16 @@
+package com.restaurant.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class AppException extends RuntimeException{
+    HttpStatus status;
+
+    public AppException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
