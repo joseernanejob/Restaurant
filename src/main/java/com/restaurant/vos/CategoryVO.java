7fa747399886471d91a60f92558be26b0bdbf51e
joseernanejob <jose.ernane.job@gmail.com>
2024-01-28 11:36:58 -0300

    feat: add categoryVO

---
+++ src/main/java/com/restaurant/vos/CategoryVO.java
@@ -0,0 +1,19 @@
+package com.restaurant.vos;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryVO {
+
+        private Long id;
+
+        private String name;
+
+}
