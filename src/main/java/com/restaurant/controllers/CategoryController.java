10eab3b9d5a8abf02dc122108c9046c7a219b147
joseernanejob <jose.ernane.job@gmail.com>
2024-01-28 12:28:31 -0300

    feat: add crud category in controller

--- src/main/java/com/restaurant/controllers/CategoryController.java
+++ src/main/java/com/restaurant/controllers/CategoryController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
@@ -18,4 +20,26 @@ public class CategoryController {
     public ResponseEntity<CategoryVO> create(@RequestBody CategoryVO data){
         return new ResponseEntity<>(service.create(data), HttpStatus.CREATED);
     }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryVO>> findAll(){
+        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryVO> findById(@PathVariable("id") Long id){
+        return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<CategoryVO> update(@RequestBody CategoryVO data){
+        return new ResponseEntity<>(service.update(data), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public  ResponseEntity<?> delete(@PathVariable("id") Long id){
+        service.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
