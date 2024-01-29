bb3bc8dc2f4b4c05dd803ba2d5ff36dbbb9ffa34
joseernanejob <jose.ernane.job@gmail.com>
2024-01-29 10:09:39 -0300

    feat: add existsProductByName and findAllByCategoryId in product repository.

--- src/main/java/com/restaurant/repostories/ProductRepository.java
+++ src/main/java/com/restaurant/repostories/ProductRepository.java
@@ -1,7 +1,14 @@
 package com.restaurant.repostories;
 
+import com.restaurant.models.Category;
 import com.restaurant.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository  extends JpaRepository<Product, Long> {
+
+    Boolean existsProductByName(String name);
+
+    List<Product> findAllByCategoryId(Long id);
 }
