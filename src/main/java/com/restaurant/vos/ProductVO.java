9e323252427eb23a909827939243d3c383a8c55a
joseernanejob <jose.ernane.job@gmail.com>
2024-01-29 10:08:16 -0300

    feat: add constructor, getter and setter.

--- src/main/java/com/restaurant/vos/ProductVO.java
+++ src/main/java/com/restaurant/vos/ProductVO.java
@@ -1,7 +1,16 @@
 package com.restaurant.vos;
 
-public class ProductVO {
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductVO {
     private Long id;
     private String name;
     private String description;
