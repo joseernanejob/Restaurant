eaa85c0b069e91ba796934bc4d9340343a3c95f4
joseernanejob <jose.ernane.job@gmail.com>
2024-01-30 12:43:32 -0300

    change: change getter, setter and constructor to lombok

--- src/main/java/com/restaurant/models/Category.java
+++ src/main/java/com/restaurant/models/Category.java
@@ -3,17 +3,15 @@ package com.restaurant.models;
 
 import com.restaurant.vos.CategoryVO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Objects;
 
-//@Getter
-//@Setter
-//@AllArgsConstructor
-//@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 @Entity
 @Table(name = "categories")
 public class Category {
@@ -25,13 +23,6 @@ public class Category {
     @Column(length = 50, nullable = false)
     private String name;
 
-    public Category() {
-    }
-
-    public Category(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
     public Category(CategoryVO data) {
         this.id = data.getId();
@@ -42,33 +33,4 @@ public class Category {
         this.name = data.getName();
     }
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Category category = (Category) o;
-        return Objects.equals(id, category.id) && Objects.equals(name, category.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
 }
