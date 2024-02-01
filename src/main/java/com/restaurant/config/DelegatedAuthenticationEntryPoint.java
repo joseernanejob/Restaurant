c494ee75b4c9dff2439193d19712426957ea3537
joseernanejob <jose.ernane.job@gmail.com>
2024-02-01 10:33:59 -0300

    feat: add DelegatedAuthenticationEntryPoint

---
+++ src/main/java/com/restaurant/config/DelegatedAuthenticationEntryPoint.java
@@ -0,0 +1,21 @@
+package com.restaurant.config;
+
+import com.restaurant.exceptions.InvalidJwtAuthenticationException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component("delegatedAuthenticationEntryPoint")
+public class DelegatedAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        throw  new InvalidJwtAuthenticationException(authException.getMessage());
+    }
+}
